c48f1eeacf8ba480d353b9d4d10e9a622d01bee7
darkanorep <jeromeperona@yahoo.com>
2025-07-09 09:19:22 +0800

    enhance API routes and create ApSpecialist22Journal model and controller for new journal type

---
+++ app/Models/ApSpecialist22Journal.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
+
+class ApSpecialist22Journal extends Model
+{
+    use HasFactory, SoftDeletes, HasMediaTrait;
+
+    protected $fillable = [
+        'adjustment_month',
+        'division_id',
+        'division_name',
+        'tag_no',
+        'transaction_date',
+        'supplier_id',
+        'supplier_code',
+        'supplier_name',
+        'entry',
+        'account_title_id',
+        'account_title_code',
+        'account_title_name',
+        'company_id',
+        'company_code',
+        'company_name',
+        'department_id',
+        'department_code',
+        'department_name',
+        'location_id',
+        'location_code',
+        'location_name',
+        'business_unit_id',
+        'business_unit_code',
+        'business_unit_name',
+        'unit_id',
+        'unit_code',
+        'unit_name',
+        'sub_unit_id',
+        'sub_unit_code',
+        'sub_unit_name',
+        'amount',
+        'description',
+        'po_no',
+        'rr_no',
+        'reference_no',
+        'quantity',
+        'unit',
+        'unit_price',
+        'voucher_number',
+        'asset_code',
+        'asset_name',
+        'service_provider_code',
+        'service_provider_name',
+        'remarks',
+        'boa',
+        'user_id',
+        'approver_id',
+        'is_approved',
+        'journal_name',
+        'journal_description',
+        'gj_number',
+        'batch_no',
+        'is_posted',
+        'posted_at',
+        'is_year_end',
+        'reason_id',
+        'reason'
+    ];
+
+    public function journals()
+    {
+        return $this->morphMany(Journal::class, 'journable');
+    }
+}
